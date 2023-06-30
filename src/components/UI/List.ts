a25425d6a999b83f99b412bf04ce1dad13407675
rweidman <36079725+rweidman@users.noreply.github.com>
2023-06-29 20:17:44 -0500

    Update List.ts

--- src/components/UI/List.ts
+++ src/components/UI/List.ts
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 
 export const UL = styled.ul`
-
+	margin: 0;
+	padding-left:25px;
+	@media only screen and (max-width: 800px) {
 		padding-left:0;
 		margin-top:25px;
-
+	}
 `;
 
 
